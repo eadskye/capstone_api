143dfec4c1b81d591aa65262a118151acb1b1796
Elizabeth Davis <eadskye@gmail.com>
2017-03-01 11:20:26 -0700

    client and person made and running, disclosure seed and migration added

--- migrations/20170301100217_person.js
+++ migrations/20170301100217_person.js
@@ -1,18 +1,23 @@
 
 'use strict';
 exports.up = function(knex, Promise) {
-  // return knex.schema.createTable('person', table => {
-  //   table.increments();
-  //   table.string('name').notNullable();
-  //   table.integer('client_id').notNullable();
-  //   table.integer('satisfaction').notNullable();
-  //   table.integer('diversity').notNullable();
-  //   table.decimal('score').notNullable();
-  //   table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
-  //   table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
-  // });
+  return knex.schema.createTable('person', table => {
+    table.increments();
+    table.string('name').notNullable();
+    table.integer('client_id').notNullable().references('id').inTable('client').onDelete('CASCADE');
+    table.integer('disclosure_id').notNullable();
+    table.integer('age_id').notNullable();
+    table.integer('gender_id').notNullable();
+    table.integer('category_id').notNullable();
+    table.integer('economic_id').notNullable();
+    table.integer('education_id').notNullable();
+    table.integer('race_id').notNullable();
+    table.integer('frequency_id').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-// return knex.schema.dropTable('person');
+return knex.schema.dropTable('person');
 };
--- seeds/02_person.js
+++ seeds/02_person.js
@@ -1,13 +1,23 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('person').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('person').insert([
+        {id: 1,
+          client_id: '1',
+          name: 'Liz',
+          disclosure_id: '1',
+          category_id: '1',
+          age_id: '1',
+          gender_id: '1',
+          economic_id:'1',
+          education_id: '1',
+          race_id: '1',
+          frequency_id:'1'
+          },
+
       ]);
     });
 };
